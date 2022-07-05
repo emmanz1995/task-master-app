e8a9b0708629afe05365a6737f97587fa83cb03f
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2022-07-05 23:36:53 +0100

    deploy app to heroku@v10

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ const path = require('path')
 app.use(express.json())
 app.use('/api/tasks', taskRoutes)
 
-app.use(express.static(path.join(__dirname, '/client/build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-})
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+// })
 
 connect()
 app.listen(PORT, () => {

0abdf06e6fdf034b5af97d2dc4fdd809cc848713
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2022-07-07 19:09:16 +0100

    exercise 11.20v10

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ const path = require('path')
 
 app.use(express.json())
 app.use('/api/tasks', taskRoutes)
+app.use(express.static('build'))
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('build'))
+// }
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
